3dbb6fbf4aaf1cf30bda0e0b4b27478ef7ab683f
keyfarel <carinsmddd@gmail.com>
2024-09-09 22:56:38 +0700

    complete ch-10

---
+++ java_ch/3. Java Stdin and Stdout II/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        scan.nextLine();
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
